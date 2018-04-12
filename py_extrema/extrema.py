1a01c7b1e2b337d317c5cd0227cafca710be8528
Corentin Cadiou <contact@cphyc.me>
2018-04-12 10:42:05 +0200

    Fixing smoothing

--- py_extrema/extrema.py
+++ py_extrema/extrema.py
@@ -244,7 +244,7 @@ class ExtremaFinder(object):
             return self.data_smooth[R]
 
         logger.debug('Smoothing at scale %.3f', R)
-        data_f = self.data_raw_f * np.exp(-self.k2 * R**2)
+        data_f = self.data_raw_f * np.exp(-self.k2 * R**2 / 2)
         self.data_smooth_f[R] = data_f
         self.data_smooth[R] = fft.irfftn(data_f)
         return self.data_smooth[R]
